945ee3745167c98d51759595c8767cd2f6f31b74
Edgardo Barria Melian <edgardo.barriam@gmail.com>
2018-12-12 21:28:05 -0300

    01 - Reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,41 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str
+      .split('')
+      .reverse()
+      .join('');
+}
+
+function reverseAlt(str) {
+  let reversed = '';
+
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+
+  return reversed;
+}
+
+function helperReverse(str) {
+  return str.split('').reduce((rev, char) => { char + rev }, '');
+}
+
+function myReverse(str) {
+  let revStr = [];
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+      revStr[left] = str[right];
+      revStr[right] = str[left];
+
+      left++;
+      right--;
+  }
+
+  return revStr.join('');
+}
 
 module.exports = reverse;
