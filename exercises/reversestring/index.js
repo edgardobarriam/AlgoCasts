14bba458160d7709c355b677af5e03312da499d5
Edgardo Barría <edgardo.barriam@gmail.com>
2024-07-15 18:36:06 -0400

    reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,15 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+
+  let reversed = "";
+
+  for (const char of str) {
+    reversed = char + reversed;
+  }
+
+  return reversed;
+}
 
 module.exports = reverse;
