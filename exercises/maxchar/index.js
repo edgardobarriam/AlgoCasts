a8791333b1c3cf79751d53d99c6a57ac3ff5207d
Edgardo Barría <edgardo.barriam@gmail.com>
2024-07-16 12:34:02 -0400

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const countObject = str.split("").reduce((map, char) => {
+    map[char] = map[char]++ || 1;
+    return map;
+  }, {});
+
+  let max = 0;
+  let maxChar;
+
+  for (const char in countObject) {
+      const count = countObject[char];
+      if (count > max) {
+        max = count;
+        maxChar = char;
+      }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
