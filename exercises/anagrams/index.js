b459e978d12139a52a7243a93d4926ec5c966bcd
Edgardo Barría <edgardo.barriam@gmail.com>
2024-07-20 22:56:29 -0400

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,46 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+/**
+ * 
+ * @param {string} stringA 
+ * @param {string} stringB 
+ * @returns {boolean}
+ */
+function anagrams(stringA, stringB) {
+  const countA = countChars(stringA.toLowerCase());
+  const countB = countChars(stringB.toLowerCase());
+
+  for (const char in countA) {
+    const a = countA[char];
+    const b = countB[char];
+    if (a != b) {
+      return false;
+    }
+  }
+
+  for (const char in countB) {
+    const a = countA[char];
+    const b = countB[char];
+    if (a != b) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+/**
+ * 
+ * @param {string} str 
+ * @returns {object}
+ */
+function countChars(str) {
+  const excludedCharacters = [' ', ',', '.', '!', '?'];
+  return str.split("").filter((char) => !excludedCharacters.includes(char)).reduce((map, char) => {
+    map[char] = map[char]++ || 1;
+    return map;
+  }, {});
+}
 
 module.exports = anagrams;
--- exercises/anagrams/test.js
+++ exercises/anagrams/test.js
@@ -8,8 +8,16 @@ test('"hello" is an anagram of "llohe"', () => {
   expect(anagrams('hello', 'llohe')).toBeTruthy();
 });
 
+test('"Hello!" is an anagram of "llohe"', () => {
+  expect(anagrams('Hello!', 'llohe')).toBeTruthy();
+});
+
 test('"Whoa! Hi!" is an anagram of "Hi! Whoa!"', () => {
   expect(anagrams('Whoa! Hi!', 'Hi! Whoa!')).toBeTruthy();
+});
+
+test('"Whoa Hi" is an anagram of "hi! whoa!"', () => {
+  expect(anagrams('Whoa Hi', 'hi! whoa!')).toBeTruthy();
 });
 
 test('"One One" is not an anagram of "Two two two"', () => {
