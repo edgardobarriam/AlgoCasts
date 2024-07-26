f8835eebb31bb76e1394000c4994d616900daaf6
Edgardo Javier Barría Melián <edgardo.barriam@gmail.com>
2024-07-26 10:15:31 -0400

    pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,21 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  const baseSize = 1 + (2 * (n-1));
+
+  for (let i = 0; i < n; i++) {
+    const numBlocks = 1 + (i*2);
+    const padding = baseSize - numBlocks;
+
+    let blocks = "";
+    for (let j = 0; j < numBlocks; j++) {
+      blocks += "#";
+    }
+
+    const line = blocks.padStart(numBlocks + padding/2 , ' ').padEnd(baseSize, ' ');
+    console.log(line);
+  }
+}
 
 module.exports = pyramid;
