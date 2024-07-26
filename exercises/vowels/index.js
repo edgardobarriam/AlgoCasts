2edcecde72142cde4c1536e8538b233e5f4dbc35
Edgardo Javier Barría Melián <edgardo.barriam@gmail.com>
2024-07-26 14:19:56 -0400

    vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,13 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  let vowelCount = 0;
+  for (const char of str.toLowerCase()) {
+    if (vowels.includes(char)) { vowelCount++ }
+  }
+  return vowelCount;
+}
 
 module.exports = vowels;
